a3bb656f2e7cc188e1aa665981741b34f7d49369
shidoshi93 <dann.ribeiroo@gmail.com>
2020-12-08 20:05:05 +0000

    Feito exercícios e os desafios 1 e 2.

---
+++ semana4/planner/index.js
@@ -0,0 +1,35 @@
+
+        
+contador = 00 //contador para incrementar um id
+function addTask(dayWeek) {
+    
+    task = document.getElementById('tarefa')
+    taskValue = task.value
+    console.log(taskValue)
+    
+    if (taskValue) {
+        contador++
+        id = 'task' + String(contador) //concatenando uma string task com o contador
+        console.log(id)
+        list = document.getElementById(dayWeek)
+        list.innerHTML += `<li id="${id}" onclick="riscar(id)">${taskValue}</li>`
+    } else {
+        alert('Insira uma tarefa.')
+    }
+    task.value = ''
+   
+}  
+
+//riscando os itens da lista
+verificar = true
+function riscar(id) {
+    if (verificar) {
+        document.getElementById(id).style = 'text-decoration: line-through'
+        verificar = false
+    } else {
+        document.getElementById(id).style = 'text-decoration: none'
+        verificar = true
+    }
+}
+
+
