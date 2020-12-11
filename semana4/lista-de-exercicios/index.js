bea9bac78cd22c079f1585dc2099e23e8ef0383d
shidoshi93 <dann.ribeiroo@gmail.com>
2020-12-11 15:13:41 +0000

    Feito primeiro exercicio de interpretaçõ de código.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,1 +1,18 @@
 //LISTA DE EXERCÍCIOS 
+
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+/* 
+1º
+
+function conversorDeMoedas
+
+A função converte dólar para real. A mesma recebe um parâmetro,
+que será o valor em dólar aa ser convertido. Dentro da função temos
+um prompt, que está pegando a cotação do dólar e guardando na variável cotacao. 
+O resultado da função é retornado '"R$" + o resultado da multiplicação do parâmetro pela cotação'.
+
+Na exibição do resultado, temos a variável meuDinheiro recebendo o resultado da função. Essa variável é passada 
+em um console.log e o resultado é exibido no console.
+*/
+
