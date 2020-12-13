68413164c8b6534efab6db9f0374ba685c957e37
shidoshi93 <dann.ribeiroo@gmail.com>
2020-12-13 14:33:40 +0000

    Feito terceiro exercício de Funções de array.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -386,3 +386,34 @@ doubleNumbers = numbers.map((num) => {
 console.log(numbers)
 console.log(doubleNumbers)
  */
+
+/* 
+3º
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+//a)
+podemEntrar = pessoas.filter((pessoa) => {
+   if(pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura >= 1.5){
+     return pessoa
+  }
+})
+
+//b)
+naoPodemEntrar = pessoas.filter((pessoa) =>{
+  if(pessoa.idade <= 14 || pessoa.idade > 60 || pessoa.altura < 1.5){
+    return pessoa
+  }
+})
+
+console.log('Array original', pessoas)
+console.log('Pessoas que podem entrar', podemEntrar)
+console.log('Pessoas que não podem entrar', naoPodemEntrar)
+*/
