7251b82b01401a9ce3d2adb770ae8a1bf3bd03b2
shidoshi93 <dann.ribeiroo@gmail.com>
2020-12-14 15:59:20 +0000

    Alterei imagens para vídeos. Redimensionei o layout e mudei algumas configurações de estilo, como mudei o header de flex para grid - para centralizar o input no centro.

--- semana5/labetube-react/labetube-react/src/App.js
+++ semana5/labetube-react/labetube-react/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
 
+  const titulo = 'Titulo do vídeo'
   
   function reproduzVideo() {
     alert("O vídeo está sendo reproduzido")
@@ -25,47 +26,59 @@ function App() {
                     <hr />
                     <li className="botoes-meunu-vertical">Originais</li>
                     <li className="botoes-meunu-vertical">Histórico</li>
+                    <li className="botoes-meunu-vertical">Seus vídeos</li>
+                    <li className="botoes-meunu-vertical">Assistir mais tarde</li>
                 </ul>
             </nav>
             
             <section className="painel-de-videos">
                 <div className="box-pagina-principal media1" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=1 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=1 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/1.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal edia2" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=2 " alt="" />
-                    <h4>Título do vídeo</h4>
+                <div className="box-pagina-principal media2" onClick={reproduzVideo}>
+                    {/*<img src="https://picsum.photos/400/400?a=2 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/2.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media3" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=3 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=3 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/3.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media4" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=4 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=4 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/4.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media5" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=5 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=5 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/5.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media6" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=6 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=6 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/6.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media7" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=7 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=7 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/7.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media8" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=8 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    {/*<img src="https://picsum.photos/400/400?a=8 " alt="" />*/}
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/8.mp4" controls className="video"></video>
+                    <h4>{titulo}</h4>
                 </div>
             </section>
         </main>
 
         <footer>
-            <h4>Oi! Eu moro no footer!</h4>
+            <p><strong>LabeTube</strong>, onde você encontra os melhores conteúdos
+                sobre o mundo, o universo e tudo mais!
+            </p>
         </footer>
     </div>
 
