5b0e87bacc1409e36455d9642216dc278684526a
shidoshi93 <dann.ribeiroo@gmail.com>
2020-12-14 23:08:47 +0000

    Retirado o onClick das divs e colocado nas tags de video.

--- semana5/labetube-react/labetube-react/src/App.js
+++ semana5/labetube-react/labetube-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   function reproduzVideo() {
     
-    alert("O vídeo está sendo reproduzido")
+    alert("O vídeo será reproduzido")
   }
 
   return (
@@ -14,7 +14,7 @@ function App() {
       <div className="tela-inteira">
         <header>
             <h1>LabeTube</h1>
-            <input type="text" placeholder="Busca" id="campoDeBusca" />
+            <input type="text" placeholder="Busca" id="campoDeBusca"/>
         </header>
 
         <main>
@@ -33,36 +33,36 @@ function App() {
             </nav>
             
             <section className="painel-de-videos">
-                <div className="box-pagina-principal media1" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/1.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media1">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/1.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media2" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/2.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media2">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/2.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media3" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/3.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media3">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/3.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media4" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/4.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media4">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/4.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media5" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/5.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media5">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/5.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media6" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/6.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media6">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/6.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media7" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/7.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media7">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/7.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media8" onClick={reproduzVideo}>
-                    <video src="https://storage.googleapis.com/future-apis.appspot.com/8.mp4" controls className="video"></video>
+                <div className="box-pagina-principal media8">
+                    <video src="https://storage.googleapis.com/future-apis.appspot.com/8.mp4" onClick={reproduzVideo} controls className="video"></video>
                     <h4>{titulo}</h4>
                 </div>
             </section>
