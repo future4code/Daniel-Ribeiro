4ca7ced9635083f619a68577e6321c5acb09b18c
shidoshi93 <dann.ribeiroo@gmail.com>
2021-01-04 17:49:01 +0000

    Feito segundo componente: Etapa2, e dado estilização básica (só para centralizar as coisas na tela).

--- semana6/labenu-forms/src/components/Etapa2.js
+++ semana6/labenu-forms/src/components/Etapa2.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const BoxEtapa2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 15px 0;
+`
+
+const InputEnsino = styled.input`
+    margin: 15px 0;
+`
 
 export default class Etapa2 extends React.Component {
     render() {
       return (
-        <div>
-          
-        </div>
+        <BoxEtapa2>
+          <h1>Etapa 2 - Informações do ensino superior</h1>
+
+          <BoxEtapa2>
+              <label>Qual o curso?</label>
+              <InputEnsino type="text" />
+          </BoxEtapa2>
+
+          <BoxEtapa2>
+              <label>Qual a unidade de ensino?</label>
+              <InputEnsino type="text" />
+          </BoxEtapa2>
+        </BoxEtapa2>
       );
     }
     
