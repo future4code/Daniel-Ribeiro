667ee02c3ffa5c5a95c7105dff2c42c16dbdd565
shidoshi93 <dann.ribeiroo@gmail.com>
2021-01-12 14:36:42 +0000

    Adicionado bibliotecas, retirado código padrão do react e alterado componente app.js de funcional para class.

---
+++ semana7/cadastro-users/cadastro-users/src/App.js
@@ -0,0 +1,18 @@
+import React from 'React'
+import axios from 'axios'
+import styled from 'styled-components'
+
+
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
+  
+}
+
+
