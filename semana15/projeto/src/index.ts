9a25135d2a314e0c6e253e36d96430c11e779e10
shidoshi93 <dann.ribeiroo@gmail.com>
2021-03-21 01:34:51 +0000

    Criado requisições de pegar saldo e adicionar saldo.

--- semana15/projeto/src/index.ts
+++ semana15/projeto/src/index.ts
@@ -8,10 +8,12 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+// pegando todos os usuários
 app.get('/users/all', (req: Request, res: Response) => {
     let errorCode: number = 400
     try {
         const newUsers: user[] = users.map(user => ({
+            id: user.id,
             nome: user.nome,
             cpf: user.cpf,
             nascimento: user.nascimento,
@@ -25,11 +27,67 @@ app.get('/users/all', (req: Request, res: Response) => {
     }
 })
 
+//Pegando saldo
+app.get('/users/saldo', (req: Request, res: Response) =>{
+    let errorCode: number = 400 
+    try {
+        let userName: string = req.query.name as string
+
+        if (!userName){
+            errorCode = 422
+            throw new Error('Invalid Parameters.')
+        } else {
+
+            const index: number = users.findIndex(user =>
+                (user.nome).toLowerCase() === (userName).toLowerCase()
+            )
+
+            const nameUser = users[index].nome
+            const saldo = users[index].saldo.toFixed(2).replace('.', ',')
+            
+            if (typeof saldo === 'string') {
+                res.status(200).send({nome: nameUser, saldo: `R$ ${saldo}`})
+                
+            } else{
+                res.status(200).send('Deu ruim.')
+            } 
+        }
+
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+// pegando o extrato de um usuário
+app.get('/users/extrato/:id', (req: Request, res: Response) =>{
+    let errorCode = 400
+    try {
+        const result: user | undefined = users.find(user =>
+            (user.id) === Number(req.params.id)
+        )
+
+        const contas = result?.extrato.map((c) =>{
+            return c
+        })
+
+        if (result) {
+            res.status(200).send(contas)
+        } else {
+            errorCode = 400
+            res.status(errorCode).send('Not Found')
+        }
+    } catch (error) {
+        res.status(errorCode).send(error)
+    }
+})
+
+// criando novo usuário
 app.post('/users', (req: Request, res: Response) => {
     let errorCode = 400
 
     try {
         const reqBody: user = {
+            id: req.body.id,
             nome: req.body.nome,
             cpf: req.body.cpf,
             nascimento: req.body.nascimento,
@@ -56,6 +114,43 @@ app.post('/users', (req: Request, res: Response) => {
 
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
+    }
+})
+
+// adicionando saldo
+app.put('/users/:id', (req: Request, res: Response) =>{
+    let errorCode: number = 400
+    type body = {
+        nome: string,
+        valor: number
+    }
+
+    const reqBody: body = {
+        nome: req.body.nome,
+        valor: Number(req.body.valor),
+    }
+
+    try {
+        const userIndex: number = users.findIndex(
+            user => user.id === Number(req.params.id)
+        )
+
+        if (userIndex === -1) {
+            errorCode = 404
+            throw new Error('Not found.')
+        }
+
+        if (!req.params.id) {
+            errorCode = 422
+            throw new Error("Invalid Parameters");
+        } else {
+            users[userIndex].saldo += Number(reqBody.valor.toFixed(2))
+        }
+
+        res.status(200).send({message: 'Saldo successfully updated', users})
+
+    } catch (error) {
+        res.status(errorCode).send(error.message)
     }
 })
 
--- semana15/projeto/src/users.ts
+++ semana15/projeto/src/users.ts
@@ -1,4 +1,5 @@
 export type user = {
+    id: number,
     nome: string,
     cpf: number,
     nascimento: string,
@@ -8,23 +9,25 @@ export type user = {
 
 export type extratoType = {
     valor: number,
-    data: "",
+    data: string,
     descricao: string
 }
 
 export const users: user[] = [
     {
+        id: 1,
         nome: "Daniel",
         cpf: 12000000000,
         nascimento: "19/05/1993",
-        saldo: 0,
-        extrato: []
+        saldo: 0.0,
+        extrato: [{valor: 15.50, data: "20/03/2021", descricao: "hot dog"},{valor: 20.50, data: "19/03/2021", descricao: "bolo"}]
     }, 
     {
+        id: 2,
         nome: "Samuel",
         cpf: 12000000015,
         nascimento: "19/05/1994",
-        saldo: 0,
+        saldo: 0.0,
         extrato: []
     }
 ]
