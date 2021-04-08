729a2a144beaffdd9d1062d3d7679e5cd06587fb
shidoshi93 <dann.ribeiroo@gmail.com>
2021-04-08 16:02:06 +0100

    Adicionado query de criar a tabela de endereços no arquivo que cria as tabelas.

--- semana18/aula52/src/endpoints/createTables/createTables.ts
+++ semana18/aula52/src/endpoints/createTables/createTables.ts
@@ -11,6 +11,20 @@ const createTables = async () =>{
                 nickname VARCHAR(255) NOT NULL
             );
         `)
+
+        await connection.raw(`
+            CREATE TABLE AddressUser (
+                id VARCHAR(255) PRIMARY KEY, 
+                cep VARCHAR(255) NOT NULL, 
+                logradouro VARCHAR(255) NOT NULL, 
+                complemento VARCHAR(255) NULL,
+                barril VARCHAR(255) NOT NULL,
+                cidade VARCHAR(255) NOT NULL,
+                uf VARCHAR(255) NOT NULL,
+                user_id VARCHAR(255) NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES User(id)
+            );
+        `)
         console.log('Tabelas criadas com sucesso!!')
     } catch (error) {
         console.log(error.message)
